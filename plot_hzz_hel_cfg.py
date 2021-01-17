05f9aed9c2f4136782d3ac2b469e018771983b7b
lucien1011 <lucien1011@gmail.com>
2021-01-17 07:05:47 -0500

    Update mass point and plot range

--- plot_hzz_hel_cfg.py
+++ plot_hzz_hel_cfg.py
@@ -16,8 +16,12 @@ namedecode = "utf-8"
 
 dataset_list = [
         sample_dict["0.002312241433103379"],
+        sample_dict["0.005355900834631777"],
+        sample_dict["0.010927157057048276"],
+        sample_dict["0.0509606966591849"],
         sample_dict["0.11241494661692496"],
-        sample_dict["0.18344811555903712"],
+        sample_dict["0.15202925850976992"],
+        sample_dict["0.19807022744383446"],
         ]
 merged_dataset_list = []
 
@@ -29,10 +33,10 @@ plots = [
         Plot("mass4l",lambda data,dataset,cfg: data["mass4l"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(25,100.,150.),),
         Plot("massZ1",lambda data,dataset,cfg: data["massZ1"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(60,0.,120.),),
         Plot("massZ2",lambda data,dataset,cfg: data["massZ2"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(30,0.,60.),),
-        Plot("pTL1",lambda data,dataset,cfg: data["pTL1"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(30,0.,60.),),
+        Plot("pTL1",lambda data,dataset,cfg: data["pTL1"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(50,0.,100.),),
         Plot("pTL2",lambda data,dataset,cfg: data["pTL2"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(30,0.,60.),),
         Plot("pTL3",lambda data,dataset,cfg: data["pTL3"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(30,0.,60.),),
-        Plot("pTL4",lambda data,dataset,cfg: data["pTL4"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(30,0.,60.),),
+        Plot("pTL4",lambda data,dataset,cfg: data["pTL4"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(20,0.,40.),),
         Plot("cosTheta1",lambda data,dataset,cfg: data["cosTheta1"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(20,-1.,1.),),
         Plot("cosTheta2",lambda data,dataset,cfg: data["cosTheta2"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(20,-1.,1.),),
         Plot("cosThetaStar",lambda data,dataset,cfg: data["cosThetaStar"],lambda data,dataset,cfg: cfg.collector.event_weight,hist=Hist1D(20,-1.,1.),),
